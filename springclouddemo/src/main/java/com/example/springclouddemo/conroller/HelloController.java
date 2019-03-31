8242ffc507b6f06be826de7f7c04565ac480a231
sunlongtao <492037876@qq.com>
2019-03-31 16:51:13 +0800

    first commit

---
+++ springclouddemo/src/main/java/com/example/springclouddemo/conroller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.springclouddemo.conroller;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @Description TODO
+ * @Author sunlongtao
+ * @Date 2019/3/31 14:54
+ **/
+@RestController
+public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @RequestMapping("hello")
+    public String index(){
+        return "Hello World";
+    }
+}
