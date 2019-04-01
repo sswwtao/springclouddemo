bd2a74d6cb7ed82b52e616e056a1578e5cc026c7
sunlongtao <492037876@qq.com>
2019-04-01 09:31:44 +0800

    处理DiscoveryClient依赖的问题

--- springclouddemo/src/main/java/com/example/springclouddemo/conroller/HelloController.java
+++ springclouddemo/src/main/java/com/example/springclouddemo/conroller/HelloController.java
@@ -1,9 +1,9 @@
 package com.example.springclouddemo.conroller;
 
-import com.netflix.discovery.DiscoveryClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
