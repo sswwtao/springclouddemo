7d1caa494444275738975354597b2158bcc9ca9a
sswwtao <492037876@qq.com>
2019-05-16 23:04:19 +0800

    1

--- springclouddemo/src/main/java/com/example/springclouddemo/conroller/HelloController.java
+++ springclouddemo/src/main/java/com/example/springclouddemo/conroller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.springclouddemo.conroller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,8 @@ public class HelloController {
     private DiscoveryClient discoveryClient;
     @RequestMapping("hello")
     public String index(){
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        logger.info("/hello,host:"+instance.getHost()+",prot"+instance.getPort()+",service_id:"+instance.getServiceId());
         return "Hello World";
     }
 }
